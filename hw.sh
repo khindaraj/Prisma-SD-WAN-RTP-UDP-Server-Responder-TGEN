ad4c4651130fa8a686c2fc1f6beaf56fa5233828
khindaraj <111604705+khindaraj@users.noreply.github.com>
2024-06-10 17:58:39 -0700

    Update hw.sh

--- hw.sh
+++ hw.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Generate random delay between 0 and 5 seconds (inclusive)
+myrand=$(awk -v min=0 -v max=5 'BEGIN{srand(); print int(min + rand() * (max - min + 1))}')
 
-myrand=$(awk -v min=0 -v max=5 'BEGIN{srand(); print min+rand()*(max-min+1)}')
+# Simulate delay
+sleep "$myrand"
 
-sleep $myrand
-
-
+# Print HTML headers
 printf "Content-type: text/html\n\n"
+
+# Print message and delay information
 printf "Hello World!\n"
-printf "Delayed for $myrand seconds\n"
+printf "Delayed for %d seconds\n" "$myrand"
