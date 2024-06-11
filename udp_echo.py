c9099ae9323e1d34aa6b9883eea523f2fd2f1eb7
khindaraj <111604705+khindaraj@users.noreply.github.com>
2024-06-10 17:54:37 -0700

    Update udp_echo.py

--- udp_echo.py
+++ udp_echo.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 import argparse
 from socket import *
 
@@ -8,23 +6,29 @@ BUFSIZE = 1024
 
 if __name__ == "__main__":
 
-    # parse arguments
-    parser = argparse.ArgumentParser()
+  # Parse arguments
+  parser = argparse.ArgumentParser()
 
-    # Allow Controller modification and debug level sets.
-    binding_group = parser.add_argument_group('Binding', 'These options change how traffic is bound.')
-    binding_group.add_argument("--ip", help="IP to listen on. (Default ALL)",
-                               type=str, default="0.0.0.0")
-    binding_group.add_argument("--port",
-                               help="Port to listen on. (Default is 6100)", type=int,
-                               default=6100)
+  # Allow Controller modification and debug level sets.
+  binding_group = parser.add_argument_group('Binding', 'These options change how traffic is bound.')
+  binding_group.add_argument("--ip", help="IP to listen on. (Default ALL)",
+                             type=str, default="0.0.0.0")
+  binding_group.add_argument("--port", help="Port to listen on. (Default is 6100)", type=int,
+                             default=6100)
 
-    args = vars(parser.parse_args())
+  args = parser.parse_args()
 
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.bind((args['ip'], args['port']))
-    print('UDP echo server {0} ready on port {1}'.format(s.getsockname()[0], s.getsockname()[1]))
-    while 1:
-        data, addr = s.recvfrom(BUFSIZE)
-        # print('server received data from %r' % (addr))
-        s.sendto(data, addr)
+  # Create a UDP socket
+  s = socket(AF_INET, SOCK_DGRAM)
+
+  # Bind the socket to the specified IP and port
+  s.bind((args.ip, args.port))
+
+  print('UDP echo server listening on {}:{}'.format(s.getsockname()[0], s.getsockname()[1]))
+
+  while True:
+    # Receive data from a client
+    data, addr = s.recvfrom(BUFSIZE)
+
+    # Echo the data back to the client
+    s.sendto(data, addr)
